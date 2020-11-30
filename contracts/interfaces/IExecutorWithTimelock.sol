e9ddca83cb3494d44345d812f539e8627e3976d7
dhadrien <hadrien@variabl.io>
2020-11-30 13:03:58 +0100

    Documenting more, interfaces

--- contracts/interfaces/IExecutorWithTimelock.sol
+++ contracts/interfaces/IExecutorWithTimelock.sol
@@ -5,25 +5,63 @@ pragma abicoder v2;
 import {IAaveGovernanceV2} from './IAaveGovernanceV2.sol';
 
 interface IExecutorWithTimelock {
+  /**
+   * @dev Getter of the current admin address (should be governance)
+   * @return The address of the current admin 
+   **/
   function getAdmin() external view returns (address);
-
+  /**
+   * @dev Getter of the current pending admin address
+   * @return The address of the pending admin 
+   **/
   function getPendingAdmin() external view returns (address);
-
+  /**
+   * @dev Getter of the delay between queuing and execution
+   * @return The delay in seconds
+   **/
   function getDelay() external view returns (uint256);
-
+  /**
+   * @dev Returns whether an action (via actionHash) is queued
+   * @param actionHash hash of the action to be checked
+   * keccak256(abi.encode(target, value, signature, data, executionTime, withDelegatecall))
+   * @return true if underlying action of actionHash is queued
+   **/
   function isActionQueued(bytes32 actionHash) external view returns (bool);
-
+  /**
+   * @dev Checks whether a proposal is over its grace period 
+   * @param governance Governance contract
+   * @param proposalId Id of the proposal against which to test
+   * @return true of proposal is over grace period
+   **/
   function isProposalOverGracePeriod(IAaveGovernanceV2 governance, uint256 proposalId)
     external
     view
     returns (bool);
-
+  /**
+   * @dev Getter of grace period constant
+   * @return grace period in seconds
+   **/
   function GRACE_PERIOD() external view returns (uint256);
-
+  /**
+   * @dev Getter of minimum delay constant
+   * @return minimum delay in seconds
+   **/
   function MINIMUM_DELAY() external view returns (uint256);
-
+  /**
+   * @dev Getter of maximum delay constant
+   * @return maximum delay in seconds
+   **/
   function MAXIMUM_DELAY() external view returns (uint256);
-
+  /**
+   * @dev Function, called by Governance, that queue a transaction, returns action hash
+   * @param target smart contract target
+   * @param value wei value of the transaction
+   * @param signature function signature of the transaction
+   * @param data function arguments of the transaction or callData if signature empty
+   * @param executionTime time at which to execute the transaction
+   * @param withDelegatecall if true, transaction you delegate call to target, else call
+   * @return the action Hash
+   **/
   function queueTransaction(
     address target,
     uint256 value,
@@ -32,7 +70,16 @@ interface IExecutorWithTimelock {
     uint256 executionTime,
     bool withDelegatecall
   ) external returns (bytes32);
-
+  /**
+   * @dev Function, called by Governance, that cancels a transaction, returns the callData executed
+   * @param target smart contract target
+   * @param value wei value of the transaction
+   * @param signature function signature of the transaction
+   * @param data function arguments of the transaction or callData if signature empty
+   * @param executionTime time at which to execute the transaction
+   * @param withDelegatecall if true, transaction you delegate call to target, else call
+   * @return the callData executed as memory bytes
+   **/
   function executeTransaction(
     address target,
     uint256 value,
@@ -41,7 +88,16 @@ interface IExecutorWithTimelock {
     uint256 executionTime,
     bool withDelegatecall
   ) external payable returns (bytes memory);
-
+  /**
+   * @dev Function, called by Governance, that cancels a transaction, returns action hash
+   * @param target smart contract target
+   * @param value wei value of the transaction
+   * @param signature function signature of the transaction
+   * @param data function arguments of the transaction or callData if signature empty
+   * @param executionTime time at which to execute the transaction
+   * @param withDelegatecall if true, transaction you delegate call to target, else call
+   * @return the action Hash
+   **/
   function cancelTransaction(
     address target,
     uint256 value,
@@ -49,5 +105,5 @@ interface IExecutorWithTimelock {
     bytes memory data,
     uint256 executionTime,
     bool withDelegatecall
-  ) external;
+  ) external returns (bytes memory);
 }
